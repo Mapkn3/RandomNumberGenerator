e85e0abd9f24ff4ee7c79affb58826791dfac4c7
Mapkn3 <lego.ben7@yandex.ru>
2018-03-21 22:30:30 +0400

    More optimization

--- main.py
+++ main.py
@@ -2,29 +2,36 @@ from functools import reduce
 from itertools import compress
 from collections import Counter
 
-def LFSR(polynom):
-    memory = [i for i in polynom]
+
+def LFSR(polynomial):
+    memory = [a for a in polynomial]
     while True:
         yield memory[0]
-        memory = memory[1:] + [reduce(lambda a, x: a^x, compress(memory, polynom))]
+        memory = memory[1:] + [reduce(lambda a, x: a ^ x, compress(memory, polynomial))]
 
-def combineLFSR(*polynoms):
-    generators = [LFSR(polynom) for polynom in polynoms]
+
+def voting_for_next_bit(values):
+    return Counter(values).most_common(1)[0][0]
+
+
+def choose_next_active(activation_flag, values):
+    return [index for index, value in enumerate(values) if value == activation_flag]
+
+
+def combine_LFSR(*polynomials):
+    generators = [LFSR(polynomial) for polynomial in polynomials]
     values = [next(generator) for generator in generators]
     while True:
-        value = Counter(values).most_common(1)[0][0]
-        yield value
-#        for ind, val in enumerate(values):
-#            if val == value
-#                values[ind] = next(generators[ind])
+        bit = voting_for_next_bit(values)
+        yield bit
+        for index in choose_next_active(bit, values):
+            values[index] = next(generators[index])
 
-#        values = [next(generators[ind]) if val == value else val for ind, val in enumerate(values)]
 
-#        for i in range(len(values)):
-#            if values[i] == value:
-#                values[i] = next(generators[i])
-        
-randomGenerator = combineLFSR([1,0,0,1,0], [1,1,1,1,1], [1,0,0,1,1])
+randomGenerator = combine_LFSR([1, 0, 0, 1, 0], [1, 1, 1, 1, 1], [1, 0, 0, 1, 1])
+f = 10
 for i in randomGenerator:
     print(i)
-
+    f -= 1
+    if f == 0:
+        break
